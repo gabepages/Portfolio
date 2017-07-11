cd1b0323b28b4f39c089e9f4038f592b289a1994
Gabe Pages <gabepages@WEB-9.local>
2017-07-11 18:02:29 -0400

    updated portfolio images

--- js/index.js
+++ js/index.js
@@ -9,4 +9,38 @@ $(document).ready(function() {
           $('#nav-background').css('opacity', "0");
         }
     });
+    var $root = $('html, body');
+    $('a').click(function(){
+      if ($(this).attr('href')[0] == "#"){
+        event.preventDefault()
+        $root.animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 60
+        }, 450);
+        return false;
+      }
+
+  });
+
+  $('.modal').modal();
+
+
+  $(document).on('scroll', function() {
+    var scrollPosition = window.scrollY;
+    if(scrollPosition > $("a[name=contact]").offset().top - 150){
+      $('#nav-contact').siblings().removeClass('active')
+      $('#nav-contact').addClass('active')
+    }else if(scrollPosition > $("a[name=portfolio]").offset().top - 61){
+      $('#nav-portfolio').siblings().removeClass('active')
+      $('#nav-portfolio').addClass('active')
+    }else if(scrollPosition > $("a[name=services]").offset().top - 61){
+      $('#nav-services').siblings().removeClass('active')
+      $('#nav-services').addClass('active')
+    }else if(scrollPosition > $("a[name=about]").offset().top - 61){
+      $('#nav-about').siblings().removeClass('active')
+      $('#nav-about').addClass('active')
+    }else{
+      $('#nav-top').siblings().removeClass('active')
+      $('#nav-top').addClass('active')
+    }
+  })
 });
