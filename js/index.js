2e1155b5d7df773dd443eb7724fe6a7a26d7afc3
Gabriel Pages <gbpages@yahoo.com>
2017-11-27 17:06:08 -0500

    responsive styling

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
 var $ = jQuery;
 
 $(document).ready(function() {
-    $(window).scroll( function(){
-        var distanceScrolled = $(this).scrollTop();
-        if(distanceScrolled > 90){
-          $('#nav-background').css('opacity', "1");
-        }else {
-          $('#nav-background').css('opacity', "0");
-        }
-    });
-    var $root = $('html, body');
-    $('a').click(function(){
-      if ($(this).attr('href')[0] == "#"){
-        event.preventDefault()
-        $root.animate({
-            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 60
-        }, 450);
-        return false;
+  
+  // Nav Background Change
+  $(window).scroll( function(){
+      var distanceScrolled = $(this).scrollTop();
+      if(distanceScrolled > 90){
+        $('#nav-background').css('opacity', "1");
+      }else {
+        $('#nav-background').css('opacity', "0");
       }
-
+  });
+    
+  // Anchor tag issue fix
+  var $root = $('html, body');
+  $('a').click(function(){
+    if ($(this).attr('href')[0] == "#"){
+      event.preventDefault()
+      $root.animate({
+          scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 60
+      }, 450);
+      return false;
+    }
   });
 
-  $('.modal').modal();
-
-
+  // Underline in Nav
   $(document).on('scroll', function() {
     var scrollPosition = window.scrollY;
     if(scrollPosition > $("a[name=contact]").offset().top - 150){
@@ -43,4 +44,19 @@ $(document).ready(function() {
       $('#nav-top').addClass('active')
     }
   })
+  
+  // Magange "valign-wrapper" class
+  if ($(window).width() <= 992) {
+    $(".services-wrapper").removeClass('valign-wrapper');
+  }
+  
+  $(window).resize(function () {
+    if ($(window).width() <= 992) {
+      $(".services-wrapper").removeClass('valign-wrapper');
+    } else if(!$(".services-wrapper").hasClass('valign-wrapper')){
+      $(".services-wrapper").addClass('valign-wrapper');
+    }
+  })
+  
+  
 });
